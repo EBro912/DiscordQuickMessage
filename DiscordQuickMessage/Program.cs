8e3db9e809ae9ce2d2588d307d111c7245a4d0d8
Ethan <ebrogames@gmail.com>
2023-02-16 18:03:21 -0500

    DM sending + Basic button functionality

--- DiscordQuickMessage/Program.cs
+++ DiscordQuickMessage/Program.cs
@@ -36,6 +36,9 @@ namespace DiscordQuickMessage
         private IServiceProvider services;
         #pragma warning restore CS8618
         private bool registerSlashCommands = false;
+        private readonly IEmote oneEmoji = Emoji.Parse(":one:");
+        private readonly IEmote twoEmoji = Emoji.Parse(":two:");
+        private readonly IEmote threeEmoji = Emoji.Parse(":three:");
 
         // the main function only calls the start function and waits for it to complete
         public static void Main(string[] args)
@@ -57,7 +60,99 @@ namespace DiscordQuickMessage
                 await interaction.ExecuteCommandAsync(context, services);
             };
 
-            // TODO: register event to handle when a user is mentioned
+            // lambda function for whenever the bot receives a message
+            client.MessageReceived += async (x) =>
+            {
+                // only run notification logic if:
+                // - The message is sent in a server text channel
+                // - If the message mentions at least one user
+                // - The message author is not a bot
+                if (x.Channel is SocketTextChannel channel && x.MentionedUsers.Count > 0 && !x.Author.IsBot)
+                {
+                    // build the embed that gets sent to each mentioned user
+                    EmbedBuilder eb = new EmbedBuilder()
+                    {
+                        Title = "You Were Mentioned",
+                        Description = $"You were mentioned by {x.Author} in {channel.Guild.Name}\n__**Content:**__\n{x.Content}\n__**Responses:**__",
+                        Color = Color.Orange
+                    };
+                    // add three inline fields to the embed
+                    // TODO: make these not placeholders
+                    eb.AddField("1", "Yes", true);
+                    eb.AddField("2", "Maybe", true);
+                    eb.AddField("3", "No", true);
+
+                    // build the three buttons on the bottom of the message
+                    // store the channel ID and message ID in the button's ID for later logic
+                    // TODO: make these functional
+                    ComponentBuilder cb = new ComponentBuilder();
+                    cb.WithButton(customId: $"{channel.Id}_{x.Id}_one", style: ButtonStyle.Success, emote: oneEmoji);
+                    cb.WithButton(customId: $"{channel.Id}_{x.Id}_two", style: ButtonStyle.Secondary, emote: twoEmoji);
+                    cb.WithButton(customId: $"{channel.Id}_{x.Id}_three", style: ButtonStyle.Danger, emote: threeEmoji);
+
+                    // loop through each mentioned user in the message
+                    foreach (SocketUser user in x.MentionedUsers)
+                    {
+                        // if the mentioned user is not a human, ignore them
+                        // TODO: ignore the user that sent the message as well, currently only allowed for testing purposes
+                        if (user.IsBot || user.IsWebhook) continue;
+
+                        // attempt to send a DM to the user containing the embed and buttons
+                        try
+                        {
+                            IDMChannel dm = await user.CreateDMChannelAsync();
+                            await dm.SendMessageAsync(embed: eb.Build(), components: cb.Build());
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Failed to send a DM to user {user}\n{e}");
+                        }
+                    }
+                }             
+            };
+
+            // lambda function for when a button is clicked
+            client.ButtonExecuted += async (x) =>
+            {
+                // extract the data from the button's ID
+                string[] data = x.Data.CustomId.Split('_');
+                // get the channel of the original message
+                SocketTextChannel? channel = await client.GetChannelAsync(ulong.Parse(data[0])) as SocketTextChannel;
+                if (channel == null)
+                {
+                    Console.WriteLine("Failed to get channel on button click!");
+                    return;
+                }
+                // get the original message
+                SocketUserMessage? msg = await channel.GetMessageAsync(ulong.Parse(data[1])) as SocketUserMessage;
+                if (channel == null)
+                {
+                    Console.WriteLine("Failed to get message on button click!");
+                    return;
+                }
+                // handle the responses depending on the button clicked
+                // this logic is temporary
+                string response = data[2] switch
+                {
+                    "one" => "Yes",
+                    "two" => "Maybe",
+                    "three" => "No",
+                    _ => throw new Exception("Unknown button ID value")
+                };
+                // build the response embed that gets sent back to the user
+                EmbedBuilder eb = new EmbedBuilder()
+                {
+                    Title = "Quick Response",
+                    Description = $"**{x.User}** responded with a quick response: {response}",
+                    Color = Color.Orange
+                };
+                // reply to the original message with the response embed
+                await msg.ReplyAsync(embed: eb.Build());
+                // let the user know that the message got sent
+                await x.RespondAsync($"Response sent successfully: {response}");
+                // remove the buttons when we are done to prevent further interaction
+                await x.Message.ModifyAsync(y => y.Components = new ComponentBuilder().Build());
+            };
 
             // reflection stuff that the library uses
             interaction = new InteractionService(client);
@@ -104,7 +199,7 @@ namespace DiscordQuickMessage
                 // we don't need a very big cache size for our purposes
                 MessageCacheSize = 10,
                 // Discord permission stuff
-                GatewayIntents = GatewayIntents.AllUnprivileged
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent | GatewayIntents.GuildMembers
             });
 
             // Dependency Injection stuff which is utilized by the library
